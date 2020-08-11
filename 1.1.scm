eb0c386f1a06cef8821b7f132fbf433fbb270b6e
hss-mateus <hss-mateus@tuta.io>
2020-08-11 18:03:31 -0300

    Redo chapter 1.1

--- 1.1.scm
+++ 1.1.scm
@@ -1,71 +1,63 @@
-(define (square x) (* x x))
-
-(define (cube x) (* x x x))
-
 ;; 1.2
-
 (/ (+ 5 4
-      (- 2 3)
-      (+ 6
-         (/ 4 5)))
+      (- 2 (- 3 (+ 6
+                   (/ 4 5)))))
    (* 3
       (- 6 2)
       (- 2 7)))
 
 ;; 1.3
+(define (sum-two-largers x y z)
 
-(define (sum-square-two-largers a b c)
-  "Sum the square root of the two largest numbers"
+  (define sum-square
+    (+ (square x)
+       (square y)
+       (square z)))
 
-  (define (sum-square a b c)
-    (+ (square a)
-       (square b)
-       (square c)))
-
-  (- (sum-square a b c)
-     (square (min a b c))))
+  (- sum-square
+     (square (min x y z))))
 
 ;; 1.7
-
 (define (sqrt x)
 
-  (define (sqrt-iter guess x)
-    (if (good-enough? guess x)
+  (define (sqrt-iter guess)
+
+        (define improve
+            (average guess (/ x guess)))
+
+        (define good-enough?
+            (< (abs (- 1 (/ guess improve)))
+               0.00001))
+
+    (if good-enough?
         guess
-        (sqrt-iter (improve guess x)
-                   x)))
+        (sqrt-iter improve)))
 
-  (define (good-enough? guess x)
-    (< (abs (- 1 (/ guess
-                    (improve guess x))))
-       0.00001))
-
-  (define (improve guess x)
-    (average guess (/ x guess)))
-
-  (define (average x y)
-    (/ (+ x y) 2))
-
-  (sqrt-iter 1.0 x))
+  (sqrt-iter 1.0))
 
 ;; 1.8
+(define (cube-root x)
 
-(define (cubic-root x)
+  (define (cube-root-iter guess)
 
-  (define (cubic-root-iter guess x)
-    (if (good-enough? guess x)
+    (define improve
+      (/ (+ (/ x (square guess))
+            (* 2 guess))
+         3))
+
+    (define good-enough?
+      (< (abs (- 1 (/ guess improve)))
+         0.00001))
+
+    (if good-enough?
         guess
-        (cubic-root-iter (improve guess x)
-                         x)))
+        (cube-root-iter improve)))
 
-  (define (good-enough? guess x)
-    (< (abs (- 1 (/ guess
-                    (improve guess x))))
-       0.00001))
+  (cube-root-iter 1.0))
 
-  (define (improve guess x)
-    (/ (+ (/ x (square guess))
-          (* 2 guess))
-       3))
+;; General functions
+(define (square x)
+  (* x x))
 
-  (cubic-root-iter 1.0 x))
+(define (average x y)
+  (/ (+ x y) 2))
